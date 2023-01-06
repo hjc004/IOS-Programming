73f72ad5c3d57a1798aeffe2d8478bf8697ff3b7
hjc004 <46177227+hjc004@users.noreply.github.com>
2023-01-06 16:35:06 +0800

    Add files via upload

---
+++ IOS十二生肖app/Passport/PassportTests/PassportTests.swift
@@ -0,0 +1,34 @@
+//
+//  PassportTests.swift
+//  PassportTests
+//
+//  Created by HHH on 2020/10/7.
+//  Copyright © 2020 HHH. All rights reserved.
+//
+
+import XCTest
+@testable import Passport
+
+class PassportTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
